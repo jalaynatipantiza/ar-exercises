b2c982eb67a4c870077412729cd571cf1797d24b
jalayna <jalaynatipantiza@gmail.com>
2020-07-21 02:52:38 +0000

    One-to-many association

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,11 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 class Store < ActiveRecord::Base
-
+  has_many :employees
 end
 
-store = Store.create(:name => "Burnaby", :annual_revenue => 300000, :mens_apparel => true, :womens_apparel =>true )
-store = Store.create(:name => "Richmond", :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true)
-store = Store.create(:name => "Gastown", :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false)
+@store1 = Store.create(:name => "Burnaby", :annual_revenue => 300000, :mens_apparel => true, :womens_apparel =>true )
+@store2 = Store.create(:name => "Richmond", :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true)
+@store3 = Store.create(:name => "Gastown", :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false)
 
 puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,19 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jalayna", last_name: "Tipantiza", hourly_rate: 40)
+@store1.employees.create(first_name: "Landon", last_name: "Tipantiza", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Jalysa", last_name: "Cancino", hourly_rate: 30)
+@store2.employees.create(first_name: "Franco", last_name: "Cancino", hourly_rate: 60)
+@store2.employees.create(first_name: "Nina", last_name: "Stubbs", hourly_rate: 10)
+
+@store4.employees.create(first_name: "Rheema", last_name: "Timmer", hourly_rate: 60)
+@store4.employees.create(first_name: "German", last_name: "Tipantiza", hourly_rate: 80)
+@store4.employees.create(first_name: "Asia", last_name: "Sighn", hourly_rate: 70)
+
